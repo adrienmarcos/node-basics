e518938593c1c768fb4d9a3886143dbc6db53ed6
Adrien Rossato <adrien.marcos@gmail.com>
2025-05-28 13:09:17 -0300

    chore: add bodyparser

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const http = require('http')
-const routes = require('./routes/routes')
-const server = http.createServer(routes)
+const express = require('express')
+const bodyParser = require('body-parser')
 
-server.listen(3000)
+const app = express()
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(adminRoutes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
+
+app.listen(3000)
