9294e57334c956cd05970e8a8cc5dd7908c8de93
Adrien Rossato <adrien.marcos@gmail.com>
2025-06-17 03:33:12 -0300

    feat: export data across routes

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,10 @@ const rootDirectory = require('../utils/path')
 const express = require('express')
 const router = express.Router()
 
+const adminData = require('./admin')
+
 router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products)
   res.sendFile(path.join(rootDirectory, 'views', 'shop.html'))
 })
 
